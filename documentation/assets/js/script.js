34c4eb9e85f898d1f89c6d67a11617b3e4078f26
Ronit Pathak <ronitpathak12345@gmail.com>
2025-03-19 13:47:51 +0530

    restfast

--- documentation/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-$('#menu-toggle').click(function(){
-  $('.navigation').addClass('show');
-  $('.overlay').addClass('show');
-});
-
-$('.overlay').click(function(){
-  $('.navigation').removeClass('show');
-  $('.overlay').removeClass('show');
-})
